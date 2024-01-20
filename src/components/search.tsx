d6e2505e3f1ee6c57fe3862c84f9d69c3dd83259
wikipop <66514885+wikipop@users.noreply.github.com>
2024-01-20 20:04:22 +0100

    initial commit

---
+++ src/components/search.tsx
@@ -0,0 +1,95 @@
+'use client'
+import Image from "next/image"
+import {useEffect, useState} from "react";
+import type {meal} from "@/types/api";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+
+type SearchProps = {
+    searchFunction: any
+}
+
+const Search = ({searchFunction}: SearchProps) => {
+    const [query, setQuery] = useState('');
+    const [results, setResults] = useState<{meals: [meal]} | null>(null);
+
+    const handleChange = (e: any) => {
+        setQuery(e.target.value);
+    }
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+
+        searchFunction(query).then((data: any) => {
+            setResults(data);
+        });
+    }
+
+    return (
+        <div className="m-12">
+            <form onSubmit={handleSubmit} className="inline-flex mb-3">
+                <Input type="text" placeholder="Search for the meal!" value={query} onChange={handleChange} className="border-2 border-black"/>
+                <Button type="submit">Search</Button>
+            </form>
+            <div className="grid grid-cols-5 gap-2">
+                {results?.meals && results.meals.map((result: any) => (
+                    <Card key={result.idMeal}>
+                        <CardHeader>
+                            <CardTitle>
+                                <AspectRatio ratio={16 / 9}>
+                                    <Image src={result.strMealThumb} alt={result.strMeal} className="rounded-md object-cover" fill />
+                                </AspectRatio>
+                            </CardTitle>
+                            <CardDescription>{result.strMeal}</CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <p className="truncate">{result.strInstructions}</p>
+                        </CardContent>
+                        <CardContent>
+                            <Sheet>
+                                <SheetTrigger><Button>Read More!</Button></SheetTrigger>
+                                <SheetContent className="!max-w-[42rem]">
+                                    <SheetHeader>
+                                        <SheetTitle>{result.strMeal}</SheetTitle>
+                                        <SheetDescription>
+                                            <AspectRatio ratio={16 / 9}>
+                                                <Image src={result.strMealThumb} alt={result.strMeal} className="rounded-md object-cover" fill />
+                                            </AspectRatio>
+                                        </SheetDescription>
+                                        <SheetDescription className="text-slate-950">
+                                            <p>
+                                                {result.strInstructions}
+                                            </p>
+                                        </SheetDescription>
+                                    </SheetHeader>
+                                </SheetContent>
+                            </Sheet>
+                        </CardContent>
+                    </Card>
+                ))}
+            </div>
+        </div>
+
+    )
+}
+
+export default Search;
